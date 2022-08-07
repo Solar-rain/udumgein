9d19cece17a45ccbe4b13019c85b06ad054e3cef
udvmge <udumge@foxmail.com>
2022-08-07 02:18:35 +0000

    demo

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping
-    public String test(){
-        return "test";
+    @GetMapping("/test1")
+    public String test1(){
+        return "test1";
+    }
+
+    @GetMapping("/test2")
+    public String test2(){
+        return "test2";
     }
 }
