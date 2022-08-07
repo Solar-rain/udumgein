ec86d46aa9c600e85a9873ce064161c58973dd1a
udumge <udumge@foxmail.com>
2022-08-07 17:35:23 +0800

    idea

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/test1")
+    @GetMapping("/test")
     public String test1(){
-        return "test1";
+        return "test";
     }
 
     @GetMapping("/test2")
