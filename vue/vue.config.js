da1c2b4921e28f011b865f1adcc57d3da52bb0e0
udvmge <udumge@foxmail.com>
2022-08-07 05:02:49 +0000

    config

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    devServer: {
+        port: 8080,
+        allowedHosts: "all",
+        client: {
+            webSocketURL: 'ws://0.0.0.0:6103/ws',
+        },
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        }
+    },
+    transpileDependencies: true
 })
